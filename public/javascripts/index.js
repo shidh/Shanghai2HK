64adf9e30a823482bea3faee2ae6291f4dc99d2a
Allen Shi <shidenghui@gmail.com>
2015-05-18 16:12:13 +0200

    add css and js lib

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,7 @@
+
+
 /**
- * Start button onclick. Start the analysis asynchronously
+ * parse json
  */
 function startOnclick(){
 	if (window.XMLHttpRequest)
@@ -12,18 +14,233 @@ function startOnclick(){
 	  }
 
 	var map;
+	$("div#progressBar").show();
 	
-	xmlhttp.open("GET","Application/start?jsonParms="+variables+"&k="+clusterPanel.getValue()+"&fileName="+fileName+"&analysisType="+document.getElementById("selectDropDown").innerHTML.split(" ")[0],true);
+	xmlhttp.open("GET","Application/start",true);
 	xmlhttp.send();
 	
 	xmlhttp.onreadystatechange=function()
-	  {
+	{
 	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
 	    {
 		  map=JSON.parse(xmlhttp.responseText);
 	      $("div#progressBar").hide();
-		  drawSeriesChart(map);
+		  //drawSeriesChart(map);
+	      alert(JSON.stringify(map));
+	
 	    }
 	  }
-	//alert(JSON.stringify(map));
 }
+
+
+/**
+ * 
+ */
+ // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.1', {'packages':['corechart',"bar"]});
+
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawSeriesChart);
+      
+      
+      google.setOnLoadCallback(drawAreaChart);
+      
+      
+      google.setOnLoadCallback(drawColumnChart);
+      
+      
+      
+      function drawColumnChart() {
+
+
+			
+    	var array = [];
+      	array[0] = ['Date', 'TotalTurnover', 'Buy', 'Sell'];
+      	i = 1;
+//        #{list items:DailyStats, as:'oneDay'}
+//        	if(i<6){}
+//	        	array[i] = [${oneDay.date}, ${oneDay.tables1.get("买入及卖出成交额 (RMB mil)")}, ${oneDay.tables1.get("买入成交额 (RMB mil)"}, ${oneDay.tables1.get("卖出成交额 (RMB mil)"} ];
+//	        	i++;
+//        	}
+//        #{/list}
+//      	for (i = 1; i <= k; i++) { 
+//      		array[i] = ["cluster"+i, parseFloat(map["X"+i]), parseFloat(map["Y"+i]), parseFloat(map["RiskRate"+i]) ,parseInt(map["S"+i])];
+//      	}
+//      	
+      	
+      	var data;
+      	if(i > 2){
+      		data = google.visualization.arrayToDataTable(array);
+      	}else{
+      		data = google.visualization.arrayToDataTable([
+	          ['Date', 'TotalTurnover', 'Buy', 'Sell'],
+	          ['2015/04/01', 1000, 400, 600],
+	          ['2015/04/02', 1170, 860, 310],
+	          ['2015/04/03', 660, 220, 440],
+	          ['2015/04/04', 1030, 530, 500]
+	        ]);
+	
+	        var options = {
+	          chart: {
+	            title: 'Company Performance',
+	            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+	          },
+	          trendlines: {
+	              0: {
+	                type: 'linear',
+	                color: 'green',
+	                lineWidth: 3,
+	                opacity: 0.3,
+	                showR2: true,
+	                visibleInLegend: true
+	              }
+	            }
+	        };
+	
+	        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+	
+	        chart.draw(data, options);
+	      }
+      	}
+      
+      
+      
+      
+      
+      function drawAreaChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('areaChart_div'));
+        chart.draw(data, options);
+      }
+      
+      
+      
+      function drawSeriesChart(map) {
+    	var k = map.K;
+    	var type = map.AnalysisType;
+    	var array = [];
+    	array[0] = ['ID', 'FactorX', 'FactorY', 'Risk Level', 'Number of cluster members'];
+
+    	for (i = 1; i <= k; i++) { 
+    		array[i] = ["cluster"+i, parseFloat(map["X"+i]), parseFloat(map["Y"+i]), parseFloat(map["RiskRate"+i]) ,parseInt(map["S"+i])];
+    	}
+    	
+    	
+    	var data;
+    	if(k > 0){
+    		data = google.visualization.arrayToDataTable(array);
+    	}else{
+    		data = google.visualization.arrayToDataTable([
+            //check https://google-developers.appspot.com/chart/interactive/docs/gallery/bubblechart#javascript
+            //ID (name) of the bubble   X coordinate, Y coordinate, Either a series ID or a value representing a color ,  Size; values in this column are mapped to actual pixel values using the sizeAxis option.
+          ['ID', 'FactorX', 'FactorY', 'Region',     'Number of cluster members'],
+          ['cluster1',    98,              1.67,      'High level education',  33739900],
+          ['cluster2',    49.84,              1.36,      'Middle level education',  81902307],
+          ['cluster3',    42.73,              2.78,      'Low level education',   79716203],
+          ['cluster4',    38.09,              4.77,      'Low level education',   31090763],
+          ['cluster5',    81.55,              2.96,      'Low level education',   7485600],
+          ['cluster6',    5,               1.54,      'Middle level education',  141850000],
+          ['cluster7',    78.09,              2.05,      'High level education',  307007000]
+          ]);
+    	}
+        //TODO
+        //change the Region to Risk alarm, red for risk cluster, green for good cluster, so on
+
+    	var options;
+    	if (type == "Performance") {
+	        options = {
+	        		//animation: {duration: 30},
+	                title: 'HrForecast Workforce Clustering Presentation',
+	                hAxis: {title: 'Factor 1'},
+	                vAxis: {title: 'Factor 2'},
+	                hAxis: {format:'#.##'},
+	                vAxis: {format:'#.##'},
+	                colorAxis: {minValue: 1, maxValue: 5, colors: ['red', 'yellow']},
+	                //chartArea: {backgroundColor, 'grey'},
+	                legend: {alignment: 'center'},
+	                sizeAxis: {minValue: 10,  maxSize: 40},
+	                chartArea: {'width': '100%', 'height': '80%'},
+	                legend: {'position': 'bottom'},
+	                bubble: {
+	                  textStyle: {
+	                    fontSize: 14,
+	                    fontName: 'STHeiti',
+	                    color: 'green',
+	                    bold: true,
+	                    auraColor: 'none'
+	                    //italic: true
+	                  }
+	                }
+	        };
+    	} else{
+	        options = {
+	        		//animation: {duration: 30},
+	                title: 'HrForecast Workforce Clustering Presentation',
+	                hAxis: {title: 'Factor 1'},
+	                vAxis: {title: 'Factor 2'},
+	                hAxis: {format:'#.##'},
+	                vAxis: {format:'#.##'},
+	                colorAxis: {colors: ['yellow', 'red']},
+	                //colorAxis: {minValue: },
+	                //colorAxis: {maxValue: },
+	                //chartArea: {backgroundColor, 'grey'},
+	                legend: {alignment: 'center'},
+	                sizeAxis: {minValue: 10,  maxSize: 40},
+	                chartArea: {'width': '100%', 'height': '80%'},
+	                legend: {'position': 'bottom'},
+	                bubble: {
+	                  textStyle: {
+	                    fontSize: 14,
+	                    fontName: 'STHeiti',
+	                    color: 'green',
+	                    bold: true,
+	                    auraColor: 'none'
+	                    //italic: true
+	                  }
+	                }
+	        };
+    	}
+
+        var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
+        function selectHandler() {
+            var selectedItem = chart.getSelection()[0];
+            if (selectedItem) {
+              var topping = data.getValue(selectedItem.row, 0);
+              
+              if (window.XMLHttpRequest)
+        	  {// code for IE7+, Firefox, Chrome, Opera, Safari
+	        	xmlhttp=new XMLHttpRequest();
+	          }
+              else
+			  {// code for IE6, IE5
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			  }
+			  xmlhttp.open("GET","Application/detail?name="+topping,false);
+			  xmlhttp.send();
+			  var map=JSON.parse(xmlhttp.responseText);
+			  
+			  showDetail(map,topping);
+	         }
+          }
+
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+        chart.draw(data, options);
+      }
+      
+
+      
+            
